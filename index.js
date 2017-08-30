6fe2ff340784ec635220835a2a3d3e981a74499b
Jamie Spittal <lastmanout@gmail.com>
2017-08-30 08:05:47 -0700

    Adding snakecase camel case fix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import enUs from './en_us.json';
+// Our ESLint doesn't like snake_case, but browsers report in snake_case so just use camelCase for
+// the import
 
 export default {
-  enUs
+  en_us: enUs
 };
