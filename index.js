951a4527b8f0479eaea133648e0d92a3422d4196
Jamie Spittal <lastmanout@gmail.com>
2017-08-30 11:13:01 -0700

    handling all options that may come from a browser

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ import enUs from './en_us.json';
 // Our ESLint doesn't like snake_case, but browsers report in snake_case so just use camelCase for
 // the import
 
+/* eslint-disable */
+/**
+ * An example of how different browsers report languages for english:
+ *
+ * Chrome/Firefox: 'en' or 'en-US'
+ * Safari/IE: 'en' or 'en-us'
+ *
+ * Because of this we need to make sure that the index.js is handling all locale options
+ */
 export default {
-  en_us: enUs
+  'en-US': enUs,
+  'en-us': enUs,
+  'en': enUS
 };
