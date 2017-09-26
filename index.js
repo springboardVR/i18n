7cba58c97502a8ec842aa03dbe043c9f4ca92335
Jamie Spittal <lastmanout@gmail.com>
2017-09-26 08:21:06 -0700

    adding polish binds to index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import enUs from './en-US.json';
 import deDe from './de-DE.json';
+import enUs from './en-US.json';
+import esEs from './es-ES.json';
+import frFr from './fr-FR.json';
+import plPL from './pl-PL.json';
+import ptBR from './pt-BR.json';
 
 /**
  * An example of how different browsers report languages for english:
@@ -10,10 +14,27 @@ import deDe from './de-DE.json';
  * Because of this we need to make sure that the index.js is handling all locale options
  */
 export default {
+  'de-DE': deDe,
+  'de-de': deDe,
+  'de': deDe,
+
   'en-US': enUs,
   'en-us': enUs,
   'en': enUs,
-  'de-DE': deDe,
-  'de-de': deDe,
-  'de': deDe
+
+  'es-ES': esEs,
+  'es-es': esEs,
+  'es': esEs,
+
+  'fr-FR': frFr,
+  'fr-fr': frFr,
+  'fr': frFr,
+
+  'pl-PL': plPL,
+  'pl-pl': plPL,
+  'pl': plPL,
+
+  'pt-BR': ptBR,
+  'pt-br': ptBR,
+  'pt': ptBR
 };
